8a7c2100313426b1a9af9ce0fb2e4a261ffa445c
phoebe <phoebeezzat@phoebes-mac.local>
2019-03-04 03:39:31 -0800

    this is great point

--- fetchcontact/ViewController.swift
+++ fetchcontact/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
-
+import Contacts
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let arraycontacts = fetchcontacts()
+        print(arraycontacts.count)
+        }
+    /*func saveContactToDocument(contact : [CNContact]){
+        let documentpath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let fileUrl = URL.init(fileURLWithPath: documentpath.appending("/MYcontacts.vcf"))
+        let data : NSData?
+        do{
+            try data = CNContactVCardSerialization.data(with: contact) as NSData
+            do{
+                try data?.write(to: fileUrl, options: [.atomicWrite])
+                print(fileUrl.absoluteString)
+            }catch{
+                print("failed to write")
+            }
+        }catch{
+            print("failed")
+        }
+    }*/
+    
+    @IBAction func fetchcontact(_ sender: UIButton) {
     }
-
-
+    func fetchcontacts() -> [CNContact]{
+        var contacts : [CNContact] = []
+        let ContactStore = CNContactStore()
+        let keys = [CNContactGivenNameKey,CNContactFamilyNameKey,CNContactPhoneNumbersKey]
+        let fetchreq = CNContactFetchRequest.init(keysToFetch: keys as [CNKeyDescriptor] )
+        do{
+        try ContactStore.enumerateContacts(with: fetchreq) { (contact, end) in
+            contacts.append(contact)
+            print(contact.givenName)
+            print(contact.phoneNumbers.first?.value.stringValue ?? "")
+            }}
+        catch{
+            print("failed to fetch")
+        }
+        return contacts
+    }
 }
 
