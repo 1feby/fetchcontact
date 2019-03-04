d4982f4fa4b6412fbe070774e133afe40de357fc
phoebe <phoebeezzat@phoebes-mac.local>
2019-03-04 11:46:32 -0800

    read from text file and get number

--- fetchcontact/ViewController.swift
+++ fetchcontact/ViewController.swift
@@ -5,51 +5,91 @@
 //  Created by phoebe on 2/28/19.
 //  Copyright © 2019 phoebe. All rights reserved.
 //
-
 import UIKit
 import Contacts
 class ViewController: UIViewController {
-
+    var fetcontacts : [CONTACTS] = []
+    var contactstr : [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let arraycontacts = fetchcontacts()
         print(arraycontacts.count)
-        }
-    /*func saveContactToDocument(contact : [CNContact]){
-        let documentpath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
-        let fileUrl = URL.init(fileURLWithPath: documentpath.appending("/MYcontacts.vcf"))
-        let data : NSData?
-        do{
-            try data = CNContactVCardSerialization.data(with: contact) as NSData
-            do{
-                try data?.write(to: fileUrl, options: [.atomicWrite])
-                print(fileUrl.absoluteString)
-            }catch{
-                print("failed to write")
-            }
-        }catch{
-            print("failed")
-        }
-    }*/
-    
-    @IBAction func fetchcontact(_ sender: UIButton) {
     }
-    func fetchcontacts() -> [CNContact]{
-        var contacts : [CNContact] = []
+    /*func saveContactToDocument(contact : [CNContact]){
+     let documentpath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+     let fileUrl = URL.init(fileURLWithPath: documentpath.appending("/MYcontacts.vcf"))
+     let data : NSData?
+     do{
+     try data = CNContactVCardSerialization.data(with: contact) as NSData
+     do{
+     try data?.write(to: fileUrl, options: [.atomicWrite])
+     print(fileUrl.absoluteString)
+     }catch{
+     print("failed to write")
+     }
+     }catch{
+     print("failed")
+     }
+     }*/
+   var filterdItemsArray = [String]()
+    func filterContentForSearchText(searchText: String) {
+        filterdItemsArray = contactstr.filter { item in
+            
+            return item.lowercased().contains(searchText.lowercased())
+        }
+    }
+    let fileURL = "/Users/phoebeezzat/Desktop/test.txt"
+    var arrayOfStrings : [String] = []
+    @IBAction func fetchcontact(_ sender: UIButton) {
+        do {
+            // Read file content
+            let contents = try NSString(contentsOfFile: fileURL, encoding: String.Encoding.utf8.rawValue)
+           let texttoread = contents as String
+            arrayOfStrings = texttoread.components(separatedBy: " ");
+            //print("bb \(arrayOfStrings.count)")
+            filterContentForSearchText(searchText: arrayOfStrings[1])
+            for p in 0...filterdItemsArray.count{
+                print(filterdItemsArray.count)
+            }
+            
+            print(" the text is \(contents)")
+        }
+        catch  {
+            print("An error took place: \(error)")
+        }
+        
+        /*var filtered = fetcontacts.filter {$0.firstName == text}
+        print(filtered.count)
+        for p in 0...filtered.count{
+            print(filtered[p].number)
+        }*/
+        
+        /*   if let foo = fetcontacts.enumerated().first(where: {$0.element.firstName == "John"}) {
+            // do something with foo.offset and foo.element
+            print(fetcontacts[foo.offset].number)
+        } else {
+            // item could not be found
+            print("couldn't found")
+        }*/
+        
+    }
+    func fetchcontacts() -> [CONTACTS]{
+        
         let ContactStore = CNContactStore()
         let keys = [CNContactGivenNameKey,CNContactFamilyNameKey,CNContactPhoneNumbersKey]
-        let fetchreq = CNContactFetchRequest.init(keysToFetch: keys as [CNKeyDescriptor] )
+        let fetchreq = CNContactFetchRequest.init(keysToFetch: keys as! [CNKeyDescriptor] )
         do{
-        try ContactStore.enumerateContacts(with: fetchreq) { (contact, end) in
-            contacts.append(contact)
-            print(contact.givenName)
-            print(contact.phoneNumbers.first?.value.stringValue ?? "")
+            try ContactStore.enumerateContacts(with: fetchreq) { (contact, end) in
+               let datacontant = CONTACTS(NAME: "\(contact.givenName) \(contact.familyName)", phoneNumber: contact.phoneNumbers.first?.value.stringValue ?? "")
+                self.fetcontacts.append(datacontant)
+                self.contactstr.append(datacontant.fullname)
+                print(contact.givenName)
+                print(contact.phoneNumbers.first?.value.stringValue ?? "")
             }}
         catch{
             print("failed to fetch")
         }
-        return contacts
+        return fetcontacts
     }
 }
-
---
+++ fetchcontact/contacts.swift
@@ -0,0 +1,19 @@
+//
+//  contacts.swift
+//  fetchcontact
+//
+//  Created by phoebe on 3/4/19.
+//  Copyright © 2019 phoebe. All rights reserved.
+//
+
+import Foundation
+
+class CONTACTS {
+    var fullname : String = ""
+    var number : String = ""
+    init(NAME : String,phoneNumber : String){
+       fullname = NAME
+        number = phoneNumber
+    }
+    
+}
